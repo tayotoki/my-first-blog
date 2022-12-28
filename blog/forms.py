06d6ca169879cabd1bb06cfc69c2989b002a90f0
ConstantinePutilov <putilov.constantine@yandex.ru>
2022-12-28 14:11:57 +0500

    Added comment form on post page

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 # from django.contrib.auth.models import User
 
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -9,6 +9,14 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
+
 
 
 # class UserRegistrationForm(forms.ModelForm):
